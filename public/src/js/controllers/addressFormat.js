09c5758805cc3c6df08997f9967a898f678fd42b
alexeiryabov <alexei@guarda.co>
2019-08-08 13:12:44 +0300

    Remove bcash address format

--- public/src/js/controllers/addressFormat.js
+++ public/src/js/controllers/addressFormat.js
@@ -1,45 +1,44 @@
 'use strict';
 
 angular.module('insight.addressFormat').controller('AddressFormatController',
-  function($scope, $rootScope, $route) {
-    var formats = [
-      {
-        name: 'CashAddr',
-        convert: bchaddr.toCashAddress
-      },
-      {
-        name: 'Legacy',
-        convert: bchaddr.toLegacyAddress
-      },
-      {
-        name: 'Copay',
-        convert: bchaddr.toBitpayAddress
-      }
-    ];
-    $scope.formats = formats;
-    $rootScope.formatAddress = function(address) {
-      try {
-        address = $rootScope.addressFormat.convert(address);
-      } catch(e) {}
-      return address;
-    };
-    $rootScope.toLegacyAddress = function(address) {
-      try {
-        address = bchaddr.toLegacyAddress(address);
-      } catch(e) {}
-      return address;
-    };
-    $rootScope.addressFormat = formats[0];
+    function($scope, $rootScope, $route) {
+        var formats = [{
+                name: 'CashAddr',
+                convert: bchaddr.toLegacyAddress
+            },
+            {
+                name: 'Legacy',
+                convert: bchaddr.toLegacyAddress
+            },
+            {
+                name: 'Copay',
+                convert: bchaddr.toBitpayAddress
+            }
+        ];
+        $scope.formats = formats;
+        $rootScope.formatAddress = function(address) {
+            try {
+                address = $rootScope.addressFormat.convert(address);
+            } catch (e) {}
+            return address;
+        };
+        $rootScope.toLegacyAddress = function(address) {
+            try {
+                address = bchaddr.toLegacyAddress(address);
+            } catch (e) {}
+            return address;
+        };
+        $rootScope.addressFormat = formats[0];
 
-    if (localStorage.getItem('insight-addressFormat')) {
-      $rootScope.addressFormat = formats.find(function(x) {
-        return x.name == localStorage.getItem('insight-addressFormat');
-      });
-    }
+        if (localStorage.getItem('insight-addressFormat')) {
+            $rootScope.addressFormat = formats.find(function(x) {
+                return x.name == localStorage.getItem('insight-addressFormat');
+            });
+        }
 
-    $scope.setAddressFormat = function(addressFormat) {
-      $rootScope.addressFormat = addressFormat;
-      localStorage.setItem('insight-addressFormat', addressFormat.name);
-      $route.reload();
-    };
-});
+        $scope.setAddressFormat = function(addressFormat) {
+            $rootScope.addressFormat = addressFormat;
+            localStorage.setItem('insight-addressFormat', addressFormat.name);
+            $route.reload();
+        };
+    });
